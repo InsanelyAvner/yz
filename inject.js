a4b89d59793669b79457d3867b1c1db33661527c
Avner <76203167+InsanelyAvner@users.noreply.github.com>
2023-05-12 16:38:42 +0800

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,5 @@
+div = document.createElement("div")
+document.body.appendChild(div)
+div.innerHTML = `
+<video controls autoplay><source src="https://yzhub.netlify.app/assets/videos/minecraft.mp4"></source></video>
+`
